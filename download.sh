caca96b1f981430ac2a6f7aff4ca0a32f1501201
lhoussou <lionel.houssou@energisme.com>
2018-12-18 12:51:24 +0100

    Add conditions in the setup & download scripts

--- download.sh
+++ download.sh
@@ -7,11 +7,11 @@
 #
 if [ ! -d .tmpdir/downloads ]; then
   mkdir .tmpdir/downloads
+  pushd ${PWD}
+  cd .tmpdir/downloads
+  curl -o apache-maven-3.6.0-bin.tar.gz https://www-us.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
+  curl -o apache-maven-3.6.0-bin.tar.gz.sha512 https://www.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz.sha512
+  #curl -o apache-maven-3.6.0-bin.tar.gz.asc https://www.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz.asc
+  if [[ "OK" != $(sha512sum -c apache-maven-3.6.0-bin.tar.gz.sha512 | grep -m 1 -o "OK") ]]; then echo "Error on checking sha512sum -c apache-maven-3.6.0-bin.tar.gz.sha512"; exit 1; fi;
+  popd
 fi
-pushd ${PWD}
-cd .tmpdir/downloads
-curl -o apache-maven-3.6.0-bin.tar.gz https://www-us.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
-curl -o apache-maven-3.6.0-bin.tar.gz.sha512 https://www.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz.sha512
-#curl -o apache-maven-3.6.0-bin.tar.gz.asc https://www.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz.asc
-if [[ "OK" != $(sha512sum -c apache-maven-3.6.0-bin.tar.gz.sha512 | grep -m 1 -o "OK") ]]; then echo "Error on checking sha512sum -c apache-maven-3.6.0-bin.tar.gz.sha512"; exit 1; fi;
-popd
--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-sudo mkdir -p /opt/automation
-sudo chown -R $(whoami):$(whoami) /opt/automation
+if [ ! -d /opt/automation ]; then
+    sudo mkdir -p /opt/automation
+    sudo chown -R $(whoami):$(whoami) /opt/automation
 
-mkdir -p /opt/automation/gitlab/{config,logs,data}
-mkdir -p /opt/automation/jenkins-master/
-mkdir -p /opt/automation/jenkins-worker
-chmod -R 775 /opt/automation
+    mkdir -p /opt/automation/gitlab/{config,logs,data}
+    mkdir -p /opt/automation/jenkins-master/
+    mkdir -p /opt/automation/jenkins-worker
+    chmod -R 775 /opt/automation
+fi
