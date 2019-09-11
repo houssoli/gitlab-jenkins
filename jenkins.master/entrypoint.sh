a9af250abc197f4146dd88af4b279b8b0bc7470d
lhoussou <lionel.houssou@energisme.com>
2019-09-12 01:47:57 +0200

    Update Jenkins plugins

--- jenkins.master/entrypoint.sh
+++ jenkins.master/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
-su -c '/etc/init.d/filebeat start' ${user}
-su -c '/sbin/tini -- /usr/local/bin/jenkins.sh' ${user}
+/sbin/tini -s -- /etc/init.d/filebeat start
+/sbin/tini -s -- /usr/local/bin/jenkins.sh
 exec "$@"
